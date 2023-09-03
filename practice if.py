601cd3280d8f26a2721df9f784bbf38db0fc6648
root <root@TylerK-1>
2023-09-03 14:57:37 -0400

    practice if.py

--- practice if.py
+++ practice if.py
@@ -3,8 +3,8 @@
 #Tyler Kirkwood
 age = (int(input("input age here: ")))
 if age > 17:
-    print("can see a rated R movie")
+    print("You can see a rated R movie")
 elif age < 17 and age > 12:
-    print("can see a rated PG-13 movie")
+    print("You can see a rated PG-13 movie")
 else:
-    print("can only see rated PG movies")
+    print("You can only see rated PG movies")
